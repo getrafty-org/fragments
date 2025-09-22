f87519b590105bb650a18e964ce43d4c623e8448
sidosera <sergey.ivanovich.sidorov@gmail.com>
2025-09-22 17:12:42 +0100

    Fix extension server path to language-server

--- vscode-extension/dist/cli.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class FragmentsCLI {
-    private static getCliPath;
-    private static getCliArgs;
-    private static getStorageFilePath;
-    static executeCommand(method: string, params?: any): Promise<any>;
-}
